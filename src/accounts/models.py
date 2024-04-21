f903dc40500d715e26030796bd3e3b9a5d1056e4
Lucas Batista dos Santos <batistalucas451@gmail.com>
2024-04-21 02:42:18 -0300

    iniciando projeto

---
+++ src/accounts/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    first_name = models.CharField(_('first name'), max_length=150, blank=True)
+    last_name = models.CharField(_('last name'), max_length=150, blank=True)
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+    is_active = models.BooleanField(_('active'), default=True)
+    is_admin = models.BooleanField(
+        _('admin status'),
+        default=False,
+        help_text=_(
+            'Designates whether the user can log into this admin site.'),
+    )
